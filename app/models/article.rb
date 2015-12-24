2b2c7ed742a8538119881f15967240e68c032507
Tom Ta <tom@futureworkz.com>
2015-12-24 13:48:03 +0700

    Add Article model and its association with Person model

--- app/models/article.rb
+++ app/models/article.rb
@@ -2,4 +2,9 @@ class Article
   include Mongoid::Document
   field :name, type: String
   field :content, type: String
+  field :published_on, type: Date
+
+  validates_presence_of :name, :content
+
+  embedded_in :person
 end
--- app/models/person.rb
+++ app/models/person.rb
@@ -4,4 +4,8 @@ class Person
   field :last_name, type: String
   field :email, type: String
   field :notes, type: String
+  field :_id, type: String, default: ->{ first_name }
+  validates_presence_of :first_name
+
+  embeds_many :articles
 end
