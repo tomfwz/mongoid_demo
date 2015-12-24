3360b12d129b402855759be8042b974d7cee6ff8
Tom Ta <tom@futureworkz.com>
2015-12-24 10:23:09 +0700

    Add People#create action

--- app/controllers/people_controller.rb
+++ app/controllers/people_controller.rb
@@ -2,4 +2,19 @@ class PeopleController < ApplicationController
   def new
     @person = Person.new
   end
+
+  def create
+    @person = Person.new(person_params)
+    if @person.save
+      redirect_to people_path, notice: 'The person has been created'
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def person_params
+    params.require(:person).permit(:first_name, :last_name, :email, :notes)
+  end
 end
